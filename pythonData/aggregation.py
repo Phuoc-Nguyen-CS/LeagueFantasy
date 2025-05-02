3d18df70cd640226fa0c9634337808d20b715146
Phuoc Nguyen <100539667+Phuoc-Nguyen-CS@users.noreply.github.com>
2025-05-01 21:49:52 -0700

    bit of work

--- pythonData/aggregation.py
+++ pythonData/aggregation.py
@@ -1,4 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv('csv/2025.csv')
-print(df)
+# Points:
+# Kills + 3
+# Assists + 2
+# Deaths - 1
+# Creep Score + 0.02
+# 10+ K/A Bonus + 2
+
+# We need to calculate their average amount of points earned per game
+# Can do Season and past Seasons
+
+def getPlayerName(csv):
+    df = pd.read_csv(csv)
+    headers = list(df.columns)
+    print(headers)
+
---
+++ pythonData/main.py
@@ -0,0 +1,4 @@
+from aggregation import *
+
+if __name__ == "__main__":
+    getPlayerName("csv/2025.csv")
