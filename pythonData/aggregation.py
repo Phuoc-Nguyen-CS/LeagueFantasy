7e7c4d447b0c1e577564d057ca9f4ca66db02762
Phuoc Nguyen <100539667+Phuoc-Nguyen-CS@users.noreply.github.com>
2025-05-05 17:39:30 -0700

    wip. finished up manipulating the data for point calculations. draftking, riot fantasy, and sleeper are the primary focus

--- pythonData/aggregation.py
+++ pythonData/aggregation.py
@@ -1,17 +1,57 @@
 import pandas as pd
+import numpy as np
 
-# Points:
-# Kills + 3
-# Assists + 2
-# Deaths - 1
-# Creep Score + 0.02
-# 10+ K/A Bonus + 2
+def read_data(csv):
+    """
+    reads the csv here to avoid re-reading in the future
 
-# We need to calculate their average amount of points earned per game
-# Can do Season and past Seasons
+    Args:
+        csv: the csv to be read
 
-def getPlayerName(csv):
-    df = pd.read_csv(csv)
-    headers = list(df.columns)
-    print(headers)
+    Returns:
+        the csv
+
+    """
+    return pd.read_csv(csv)
+
+def calc_draft_kings_points(df):
+    """
+    Calculate Points based on these parameters:
+
+    kills                   + 3
+    assists                 + 2
+    death                   - 1
+    cs                      - 0.02 per CS
+    first_blood             + 2
+    first_turrent           + 1
+    dragon_kill             + 0.3 points per drag kill divided by number of players(5)
+    first_dragon
+    baron_kill              + 2
+    result(win)             + 2
+
+    ** Noted additional points **
+    10 + Kills/Assists      + 2
+    Win < 30 min Bonus      + 2
+
+    Args:
+        df: dataframe
+
+    Returns:
+        dataframe with draft kings points
+
+    """
+
+    df['draft_kings_score'] = (
+        df['kills'] * 3 +
+        df['assists'] * 2 -
+        df['deaths'] * 1 +
+        df['total cs'] * 0.02 +
+        df['firstbloodkill'] * 2 +
+        df['firsttower'] * 2 +
+        (df['dragons'] * 0.3) / 5
+    )
+
+    df['draft_kings_score'] += np.where(df['kills'] >= 10, 2, 0)
+
+
 
--- pythonData/main.py
+++ pythonData/main.py
@@ -1,4 +1,11 @@
 from aggregation import *
 
 if __name__ == "__main__":
-    getPlayerName("csv/2025.csv")
+    df = read_data("csv/2025.csv")
+
+    team_stats = df.groupby(['gameid', 'teamname']).agg(
+        team_dragons=('dragons', 'sum'),
+        team_firsttower=('firsttower', 'max')
+    ).reset_index()
+
+    df = df.merge(team_stats, on=['gameid', 'teamname'], how='left')
