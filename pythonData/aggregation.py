d4141d2a232b990547a2df2b3618ccc37978c784
Phuoc Nguyen <phuocnguyen102800@gmail.com>
2025-05-12 17:44:41 -0700

    thin up the data

--- pythonData/aggregation.py
+++ pythonData/aggregation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+
 def read_data(csv):
     """
     reads the csv here to avoid re-reading in the future
@@ -12,7 +13,9 @@ def read_data(csv):
         the csv
 
     """
-    return pd.read_csv(csv)
+    df = pd.read_csv(csv, dtype={"url": str})
+    df = df[df['datacompleteness'] == 'complete'].copy()
+    return df
 
 
 def get_stats(df):
@@ -30,7 +33,8 @@ def get_stats(df):
     """
 
     # Combine elemental and elder dragons
-    df['total_dragons'] = df['dragons'].fillna(0) + df['elders'].fillna(0)
+    # df['total_dragons'] = df['dragons'].fillna(0) + df['elders'].fillna(0)
+    df.loc[:, 'total_dragons'] = df['dragons'].fillna(0) + df['elders'].fillna(0)
 
     # Group team stats per game
     team_stats = df.groupby(['gameid', 'teamname']).agg(
@@ -45,6 +49,66 @@ def get_stats(df):
     df['gamelength_minutes'] = df['gamelength'] / 60
 
     return df
+
+
+def regions(df):
+    """
+    returns the necessary region/league we are trying to get data for
+
+    Removes all:
+        * where "league" is LPL
+        * url
+
+
+    Args:
+        df: the dataframe to be manipulated
+
+    Returns:
+        df: a lighter dataframe
+
+    """
+    df = df[df['league'].isin(['LCK', 'LEC'])].copy()
+
+    return df
+
+
+def needed_columns(df):
+    """
+    Returns only the columns needed to calculate the fantasy points
+
+    Args:
+        df: dataframe
+
+    Returns:
+        fp_df: only the necessary information is held here
+    """
+
+    fp_df = df[[
+        'gameid',
+        'league',
+        'year',
+        'split',
+        'patch',
+        'participantid',
+        'side',
+        'position',
+        'playername',
+        'teamname',
+        'gamelength',
+        'result',
+        'kills',
+        'assists',
+        'deaths',
+        'total cs',
+        'towers',
+        'dragons',
+        'elders',
+        'firstbloodkill',
+        'barons'
+    ]].copy()
+
+    return fp_df
+
 
 def calc_draft_kings_points(df):
     """
@@ -75,24 +139,22 @@ def calc_draft_kings_points(df):
 
     # Calculate player's individual points
     df['draft_kings_score'] = (
-        df['kills'] * 3 +
-        df['assists'] * 2 -
-        df['deaths'] * 1 +
-        df['total cs'] * 0.02 +
-        df['firstbloodkill'] * 2
+            df['kills'] * 3 +
+            df['assists'] * 2 -
+            df['deaths'] * 1 +
+            df['total cs'] * 0.02 +
+            df['firstbloodkill'] * 2
     )
+
     df['draft_kings_score'] += np.where((df['kills'] >= 10) | (df['assists'] >= 10), 2, 0)
 
     # Calculate team's score
-    df['draft_kings_score'] = (
-        df['team_towers'] * 1 +
-        df['team_dragons'] * 2 +
-        df['barons'] * 3
+    df['draft_kings_score'] += (
+            df['team_towers'] * 1 +
+            df['team_dragons'] * 2 +
+            df['barons'] * 3
     )
     df['draft_kings_score'] += np.where(df['result'] == 1, 2, 0)
     df['draft_kings_score'] += np.where(df['gamelength_minutes'] <= 30, 2, 0)
 
     return df
-
-
-
--- pythonData/main.py
+++ pythonData/main.py
@@ -1,12 +1,25 @@
 from aggregation import *
+import pandas as pd
+
+pd.set_option('display.max_rows', 30)
+pd.set_option('display.max_columns', 30)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
 
 if __name__ == "__main__":
     df = read_data("csv/2025.csv")
+    df = regions(df)
+    df.drop("url", axis=1, inplace=True)
 
+    # Assign only the columns needed to make the calculations to reduce clutter
+    fp_df = needed_columns(df)
     # Get stats that are needed and assign to each corresponding player
-    df = get_stats(df)
+    fp_df = get_stats(fp_df)
 
     # Calculate fantasy points for draft king
-    df = calc_draft_kings_points(df)
+    fp_df = calc_draft_kings_points(fp_df)
+    fp_df = fp_df[fp_df['position'] != 'team'].copy()
 
-    df.to_csv('output.csv', index=False)
+    print(fp_df.sort_values('draft_kings_score', ascending=False))
+    fp_df.to_csv('output.csv', index=False)
